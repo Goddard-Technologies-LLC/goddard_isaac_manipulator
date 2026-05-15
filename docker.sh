a54eecfa7cea103aa5db11086d7207e24910b112
Sean Yeatts <syeatts@goddardtech.com>
2026-05-15 16:31:13 -0400

    updated volume path

--- docker.sh
+++ docker.sh
@@ -3,5 +3,5 @@
 # Attach this project to the docker container as a bound volume ("kiosk-controller")
 cd ${ISAAC_ROS_WS}/src/isaac_ros_common && ./scripts/run_dev.sh \
     -d ${ISAAC_ROS_WS} \
-    --docker_arg "-v ${HOME}/GAR006-Sandbox/sandbox-03:/kiosk-controller" \
+    --docker_arg "-v ${HOME}/GAR006-Kiosk-Controller:/kiosk-controller" \
     --skip_image_build

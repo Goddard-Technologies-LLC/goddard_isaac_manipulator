83c21fa037321aeed2e86c771f4e57119760718b
Sean Yeatts <syeatts@goddardtech.com>
2026-05-15 23:30:05 -0400

    added workaround for setuptools_scm versioning issue

--- launch.sh
+++ launch.sh
@@ -2,6 +2,14 @@
 
 # Launches the top-level ROS package
 
+# BUG: Packages that depend on curobo will encounter a setuptools_scm versioning
+# issue as a result of our submodule setup. There is a pathing issue, where .gitmodules
+# paths work with our local environment, but not once we've entered the Docker
+# container. SCM supports forced versioning - we're just declaring the appropriate
+# version (see source/workspaces/nvidia/src/isaac_ros_cumotion/curobo_core/curobo/src/curobo/__init__.py)
+
+export SETUPTOOLS_SCM_PRETEND_VERSION=0.7.5
+
 # Ensure the ROS underlay is sourced
 source /opt/ros/${ROS_DISTRO}/setup.sh
 

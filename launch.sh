7691f8320c14af043c6bee74eceee902442fd7bc
Sean Yeatts <syeatts@goddardtech.com>
2026-05-15 22:55:17 -0400

    path bugfix

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 # Launches the top-level ROS package
 
 # Ensure the ROS underlay is sourced
-source /opt/ros/${ROS_DISTRO}/install/setup.sh
+source /opt/ros/${ROS_DISTRO}/setup.sh
 
 # Ensure all of the project overlays are sourced
 source ./source/scripts/source.sh

d5281d54d45de1377de5f7d6c9cc6ad150892ca6
Sean Yeatts <syeatts@goddardtech.com>
2026-05-15 21:12:50 -0400

    updated paths to improve robustness

--- docker.sh
+++ docker.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Attach this project to the docker container as a bound volume ("kiosk-controller")
+# Launches the Isaac ROS docker container
+
+# Resolve this script's directory
+SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Attach this project to the docker container as a bound volume ("kiosk")
 cd ${ISAAC_ROS_WS}/src/isaac_ros_common && ./scripts/run_dev.sh \
     -d ${ISAAC_ROS_WS} \
-    --docker_arg "-v ${HOME}/GAR006-Kiosk-Controller:/kiosk-controller" \
+    --docker_arg "-v ${SCRIPT_DIRECTORY}:/kiosk" \
     --skip_image_build

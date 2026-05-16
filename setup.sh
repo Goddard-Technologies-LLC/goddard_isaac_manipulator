a99c38154db1b721c1e43f86bee92fc9f86363f1
Sean Yeatts <syeatts@goddardtech.com>
2026-05-15 22:11:16 -0400

    removed install/build steps

--- setup.sh
+++ setup.sh
@@ -4,6 +4,15 @@
 
 echo "Initializing project..."
 
+# Resolve this script's directory
+SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Generate local config
+echo "generating local config"
+CONFIG_DEFAULT="config/config.default.yaml"
+CONFIG_LOCAL="config/config.local.yaml"
+cp ${CONFIG_DEFAULT} ${CONFIG_LOCAL}
+
 # Add docker config for realsense
 echo "generating docker config"
 isaac_config_path=${ISAAC_ROS_WS}/src/isaac_ros_common/scripts/.isaac_ros_common-config
@@ -13,19 +22,6 @@ echo CONFIG_IMAGE_KEY=ros2_humble.realsense > ${isaac_config_path}
 # Download NVIDIA assets
 source source/scripts/assets.sh
 
-# Install dependencies
-source source/scripts/install.sh
-
-# Build & source project
-source source/scripts/build.sh
-source source/scripts/source.sh
-
-# Instantiate local config
-echo "generating local config"
-CONFIG_DEFAULT="config/config.default.yaml"
-CONFIG_LOCAL="config/config.local.yaml"
-cp ${CONFIG_DEFAULT} ${CONFIG_LOCAL}
-
 # Return to project root
-cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd ${SCRIPT_DIRECTORY}
 echo "setup complete!"
